6ed5ea2141ae1283934349ba8125502aa9f473c0
ZFlei <zoe.flei@gmail.com>
2015-02-08 12:32:12 -0500

    Added R Code and PNG for plot 4
    Documentation for plot 4 (includes 4 plots of variables over a 48 hour
    range - 01-02-2007 to 02-02-2007). Top left is global active power (kW)
    vs time (also plot2). Bottom left is Energy sub metering vs. time for 3
    energy sub metering variables (also found in plot3). Top right
    represents Voltage vs. datetime. Bottom right represents
    Global_reactive_power vs. datetime.
    R  Code and PNG image added.

---
+++ plot4.R
@@ -0,0 +1,38 @@
+## Get the full dataset
+ePowerConsumption <- read.table("./edaData/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors=FALSE)
+ePowerConsumption$Date <- as.Date(ePowerConsumption$Date, format = "%d/%m/%Y")
+
+##Subset data for date ranges: 2007-02-01 to 2007-02-02
+epConsumptionSubset <- subset(ePowerConsumption, as.Date(Date)>= "2007-02-01" & as.Date(Date) <= "2007-02-02")
+datetime <- paste(as.Date(epConsumptionSubset$Date), epConsumptionSubset$Time)
+epConsumptionSubset$Datetime <- as.POSIXct(datetime)
+
+##Plot 4 (2x2 matrix)
+par(mfrow =c(2,2))
+
+##topleft: plot from plot2
+plot(epConsumptionSubset$Global_active_power ~ epConsumptionSubset$Datetime, 
+     type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+
+##top right: Voltage v. "datetime"
+plot(epConsumptionSubset$Voltage ~ epConsumptionSubset$Datetime, 
+     type = "l", ylab = "Voltage", xlab = "datetime")
+
+##bottomleft: plot from plot3
+with(epConsumptionSubset, {
+  plot(Sub_metering_1~Datetime, type = "l",
+       ylab = "Energy sub metering", xlab = "")
+  lines(Sub_metering_2~Datetime, col = "Red")
+  lines(Sub_metering_3~Datetime, col = "Blue")
+})
+legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.6)
+
+##bottomright:Global_reactive_power vs. "datetime"
+plot(epConsumptionSubset$Global_reactive_power ~ epConsumptionSubset$Datetime, 
+     type = "l", ylab = "Global_reactive_power", xlab = "datetime")
+
+
+##Save and close file
+dev.copy(png, file = "plot4.png", height = 480, width = 480)
+dev.off()
